e2bd05c9860fe854a6fdbd9c7be447077c9392db
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2023-04-04 21:02:57 +0530

    create class TransactionService

---
+++ Lab2Algo_Question_1/src/com/glearning/assignment1/service/TransactionService.java
@@ -0,0 +1,14 @@
+package com.glearning.assignment1.service;
+
+public class TransactionService {
+	public int checkTransactionsTarget(int array[],int target) {
+		int totalCount = 0;
+		for(int i = 0; i < array.length; i++) {
+			totalCount += array[i];
+			if(totalCount >= target) {
+				return i+1;
+			}
+			}
+		return -1;
+	}
+}
